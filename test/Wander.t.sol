dd8ead1d11576ff3f32de2626383aafb44b5c4d8
Henrik Kvamme <henrik@kvamme.xyz>
2023-09-23 05:33:04 +0200

    Initial contract

---
+++ test/Wander.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Wander.sol";
+
+contract WanderTest is Test {
+    Wander public wander;
+
+    function setUp() public {}
+}
